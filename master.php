4bd0157231cab9629abedf68ed8ec3d65a02adcf
fullstack82 <fullstack8210@gmail.com>
2021-01-27 12:15:38 +0100

    first commit

---
+++ master.php
@@ -0,0 +1,8 @@
+<?php 
+//Una variable es un contenedor de info, puedo guardar cualquier datos dentro de ella
+$mi_primera_variable = "hola mundo desde una variable";
+
+
+echo '<h1>'.$mi_primera_variable.'</h1>';
+
+?>
